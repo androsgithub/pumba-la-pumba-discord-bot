4dc05144258615a4863bc5687d7fe9bc30181a82
androsgithub <andreemaprinc@gmail.com>
2024-11-17 16:21:41 -0300

    refact: removed the useless props, and some useless and duplicate variables, also add the new component of song card

--- src/events/music/onPlay.ts
+++ src/events/music/onPlay.ts
@@ -1,32 +1,70 @@
-import { Player, Track } from "discord-player";
+import { GuildQueue, Player, Track, useQueue } from "discord-player";
 import { YoutubeiExtractor } from "discord-player-youtubei";
-import { Message, OmitPartialGroupDMChannel } from "discord.js";
-import { validateURL, getInfo } from "ytdl-core";
+import { EmbedBuilder, Message, OmitPartialGroupDMChannel } from "discord.js";
+import { validateURL, getInfo, videoInfo } from "ytdl-core";
+import { transformMillisecondsInTimeText } from "../../utils/transform-time";
+import { volume } from "../../../config.json";
+import { player } from "../..";
+import { songEmbed } from "../../components/embed";
 
 export async function play(
-  voiceChannelId: string,
-  songQuery: string,
-  player: Player,
+  url: string,
+  queue: GuildQueue | null,
   message: OmitPartialGroupDMChannel<Message<boolean>>
 ) {
-  let video: Track | string | null = null;
+  const voiceChannelId = message.member!.voice.channelId ?? "";
+  let video: Track = new Track(player, {});
   try {
-    player.extractors.register(YoutubeiExtractor, {});
-    player.extractors.loadDefault();
-    if (validateURL(songQuery)) {
-      video = songQuery.toString();
+    await player.extractors.register(YoutubeiExtractor, {});
+    await player.extractors.loadDefault();
+
+    if (validateURL(url)) {
+      let _data = await getInfo(url.toString());
+
+      video.url = url.toString();
+      video.title = _data.videoDetails.title ?? "Titulo não encontrado.";
+      video.description =
+        _data.videoDetails.description ?? "Descrição não encontrada";
+      video.author = _data.videoDetails.author.name ?? "Autor não encontrado.";
+      video.duration = _data.videoDetails.lengthSeconds ?? 0;
+      video.thumbnail =
+        _data.videoDetails.thumbnails[0].url ??
+        "https://lightwidget.com/wp-content/uploads/localhost-file-not-found.jpg";
     } else {
-      video = (await player.search(songQuery)).tracks[0];
+      video = (await player.search(url)).tracks[0];
     }
 
     await player.play(voiceChannelId, video, {
-      nodeOptions: { volume: 100 },
+      nodeOptions: { volume },
     });
 
-    await message.channel.send(`Tocando: **${video}**`);
+    const embed = songEmbed(
+      video.title,
+      video.description,
+      { name: video.author },
+      [
+        {
+          name: "Duração da musica",
+          value: transformMillisecondsInTimeText(video.durationMS),
+          inline: true,
+        },
+        {
+          name: "Duração da playlist",
+          value: transformMillisecondsInTimeText(
+            queue?.currentTrack?.durationMS
+              ? video.durationMS + queue.estimatedDuration
+              : video.durationMS
+          ),
+        },
+      ],
+      { url: video.thumbnail },
+      message.author
+    );
+
+    await message.channel.send({ embeds: [embed] });
   } catch (e: any) {
     console.error("erro ao tocar musica");
-    console.error(e.message);
+    console.error(e);
     return;
   }
 }
