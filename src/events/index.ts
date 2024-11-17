d7fc87d200d06b631822bd841b7f9cee2741d460
androsgithub <andreemaprinc@gmail.com>
2024-11-17 04:33:04 -0300

    feat:add new features, like search,time, and added some awaits

--- src/events/index.ts
+++ src/events/index.ts
@@ -6,6 +6,8 @@ import { getVoiceConnection } from "@discordjs/voice";
 import { YoutubeiExtractor } from "discord-player-youtubei";
 import { skip } from "./music/onSkip";
 import { seek } from "./music/onSeek";
+import { transformMillisecondsInTimeText } from "../utils/transform-time";
+import { search } from "./music/onSearch";
 
 export async function onEvent(
   message: OmitPartialGroupDMChannel<Message<boolean>>,
@@ -26,14 +28,46 @@ export async function onEvent(
           .trim();
         play(userVoiceChannelId, q, player, message);
       } else {
-        message.channel.send("Você precisa estar conectado a um canal de voz.");
+        await message.channel.send(
+          "Você precisa estar conectado a um canal de voz."
+        );
+      }
+      break;
+    case args[0] == "search":
+      if (message.member?.voice.channelId) {
+        const q = args
+          .slice(1, args.length)
+          .toString()
+          .replaceAll(",", " ")
+          .trim();
+        search(userVoiceChannelId, q, message, player);
+      } else {
+        await message.channel.send(
+          "Você precisa estar conectado a um canal de voz."
+        );
+      }
+
+      break;
+    case args[0] == "time":
+      if (message.member?.voice.channelId) {
+        await message.channel.send(
+          `${transformMillisecondsInTimeText(
+            queue?.node.estimatedPlaybackTime || 0
+          )} de ${transformMillisecondsInTimeText(
+            queue?.currentTrack?.durationMS || 0
+          )}`
+        );
+      } else {
+        await message.channel.send("Nenhum som está tocando!");
       }
       break;
     case args[0] == "seek":
       if (message.member?.voice.channelId) {
         seek(queue, message, args[1]);
       } else {
-        message.channel.send("Você precisa estar conectado a um canal de voz.");
+        await message.channel.send(
+          "Você precisa estar conectado a um canal de voz."
+        );
       }
       break;
     case args[0] == "skip":
@@ -41,10 +75,10 @@ export async function onEvent(
       break;
     case args[0] == "stop":
       if (client.voice.adapters.size > 0) {
-        message.channel.send("O som foi parado!");
+        await message.channel.send("O som foi parado!");
         player.destroy();
       } else {
-        message.channel.send("Nenhum som está tocando!");
+        await message.channel.send("Nenhum som está tocando!");
       }
       break;
   }
