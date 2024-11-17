18627a1dcb32b2be76d3e3b5da7fc86337cd5945
androsgithub <andreemaprinc@gmail.com>
2024-11-17 16:17:42 -0300

    feat: create a component to handle with menu

---
+++ src/components/select-menu.ts
@@ -0,0 +1,36 @@
+import {
+  ActionRowBuilder,
+  APISelectMenuOption,
+  APIStringSelectComponent,
+  StringSelectMenuBuilder,
+  StringSelectMenuOptionBuilder,
+} from "discord.js";
+
+export function select(selectMenu: APIStringSelectComponent) {
+  return new ActionRowBuilder<StringSelectMenuBuilder>({
+    components: [selectMenu],
+  });
+}
+
+export function selectMenu(
+  custom_id: string,
+  placeholder: string,
+  options: APISelectMenuOption[]
+) {
+  return new StringSelectMenuBuilder({
+    custom_id,
+    placeholder,
+    options: options.splice(0, 25),
+  }).toJSON();
+}
+export function selectMenuOption(
+  label: string,
+  description: string,
+  value: string
+) {
+  return new StringSelectMenuOptionBuilder({
+    label: label.substring(0, 100),
+    description: description.substring(0, 50),
+    value: value,
+  }).toJSON();
+}
