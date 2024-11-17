05d095df9f1601473c4ce9afb423b1d772aa104d
androsgithub <andreemaprinc@gmail.com>
2024-11-17 16:18:40 -0300

    refact: remove the blindless and useless code, transfer the responsibilite of ifs to the event functions

--- src/events/index.ts
+++ src/events/index.ts
@@ -1,85 +1,56 @@
 import { Message, OmitPartialGroupDMChannel } from "discord.js";
 import { play } from "./music/onPlay";
-import { ExtendedClient } from "../structs/extended-client";
-import { Player, Track, useQueue } from "discord-player";
-import { getVoiceConnection } from "@discordjs/voice";
-import { YoutubeiExtractor } from "discord-player-youtubei";
+import { GuildQueue, useQueue } from "discord-player";
 import { skip } from "./music/onSkip";
 import { seek } from "./music/onSeek";
-import { transformMillisecondsInTimeText } from "../utils/transform-time";
 import { search } from "./music/onSearch";
+import { time } from "./music/onTime";
+import { stop } from "./music/onStop";
+import { player } from "..";
+import { restart } from "./functions/onRestart";
+import { config } from "../utils/configs";
+import { setVolume } from "./configs/onSetVolume";
 
 export async function onEvent(
-  message: OmitPartialGroupDMChannel<Message<boolean>>,
-  client: ExtendedClient,
-  player: Player
+  message: OmitPartialGroupDMChannel<Message<boolean>>
 ) {
-  const userVoiceChannelId = message.member!.voice.channelId ?? "";
+  const isUserAdmin = message.member?.permissions.has("Administrator") || false;
   const args = message.content.trim().split(" ");
-  const queue = useQueue(message.guild!.id);
+  const queue: GuildQueue | null = player.queues.get(message.guild!.id);
 
   switch (true) {
+    //music
     case args[0] == "play" || args[0] == "p":
-      if (message.member?.voice.channelId) {
-        const q = args
-          .slice(1, args.length)
-          .toString()
-          .replaceAll(",", " ")
-          .trim();
-        play(userVoiceChannelId, q, player, message);
-      } else {
-        await message.channel.send(
-          "Você precisa estar conectado a um canal de voz."
-        );
-      }
+      const url = args[1];
+      play(url, queue, message);
       break;
-    case args[0] == "search":
-      if (message.member?.voice.channelId) {
-        const q = args
-          .slice(1, args.length)
-          .toString()
-          .replaceAll(",", " ")
-          .trim();
-        search(userVoiceChannelId, q, message, player);
-      } else {
-        await message.channel.send(
-          "Você precisa estar conectado a um canal de voz."
-        );
-      }
 
+    case args[0] == "search":
+      search(args, queue, message);
       break;
+
     case args[0] == "time":
-      if (message.member?.voice.channelId) {
-        await message.channel.send(
-          `${transformMillisecondsInTimeText(
-            queue?.node.estimatedPlaybackTime || 0
-          )} de ${transformMillisecondsInTimeText(
-            queue?.currentTrack?.durationMS || 0
-          )}`
-        );
-      } else {
-        await message.channel.send("Nenhum som está tocando!");
-      }
+      time(queue, message);
       break;
+
     case args[0] == "seek":
-      if (message.member?.voice.channelId) {
-        seek(queue, message, args[1]);
-      } else {
-        await message.channel.send(
-          "Você precisa estar conectado a um canal de voz."
-        );
-      }
+      seek(args[1], queue, message);
       break;
+
     case args[0] == "skip":
       skip(queue, message);
       break;
+
     case args[0] == "stop":
-      if (client.voice.adapters.size > 0) {
-        await message.channel.send("O som foi parado!");
-        player.destroy();
-      } else {
-        await message.channel.send("Nenhum som está tocando!");
-      }
+      stop(queue, message);
+      break;
+
+    //configs
+    case args[0] == "volume" || args[0] == "vol":
+      setVolume(args[1], queue, message, isUserAdmin);
+      break;
+    case args[0] == "restart" && isUserAdmin:
+      restart(message);
       break;
   }
 }
